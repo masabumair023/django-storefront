b76508027c531bd574756593d1c967895b89e540
Masab Umair <Masab.Umair@mtp.tech>
2022-08-02 17:49:07 +0500

    Add api_view for product details

--- store/views.py
+++ store/views.py
@@ -1,9 +1,15 @@
 from urllib import response
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 # Create your views here.
 
-
+@api_view(['GET'])
 def product_list(request):
-    return HttpResponse("Ok")
+    return Response("Ok")
+
+@api_view(['GET'])
+def product_details(request, id):
+    return Response(id)
