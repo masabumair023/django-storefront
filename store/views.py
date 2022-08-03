472b7e19fa683df6e473a9f30e110aa421ba8c81
Masab Umair <Masab.Umair@mtp.tech>
2022-08-03 18:19:30 +0500

    Add get all collection api

--- store/views.py
+++ store/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import get_object_or_404
+from django.db.models import Count
 
-from .models import Product
-from .serializer import ProductSerializer
+from .models import Collection, Product
+from .serializer import CollectionSerializer, ProductSerializer
 
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+
+from store import serializer
 
 # Create your views here.
 
@@ -15,11 +18,19 @@ def product_list(request):
         serializer = ProductSerializer(product_queryset, many=True)
         return Response(serializer.data)
     elif request.method == 'POST':
-        serializer = ProductSerializer(request.data)
-        return Response("This is a post request")
+        print(request.data)
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        return Response(serializer.validated_data)
 
 @api_view(['GET'])
 def product_details(request, id):
     product = get_object_or_404(Product, pk=id)
     serializer = ProductSerializer(product)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def collection_list(request):
+    collections = Collection.objects.annotate(product_count=Count('product')).all()
+    serializer = CollectionSerializer(collections, many=True)
     return Response(serializer.data)
