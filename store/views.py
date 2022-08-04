e082d87a5391e01049585d5d6fd82c3efefe18c7
Masab Umair <Masab.Umair@mtp.tech>
2022-08-04 14:31:06 +0500

    Implement class based views

--- store/views.py
+++ store/views.py
@@ -1,3 +1,4 @@
+from logging import raiseExceptions
 from django.shortcuts import get_object_or_404
 from django.db.models import Count
 
@@ -6,55 +7,57 @@ from .serializer import CollectionSerializer, ProductSerializer
 
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-
-from store import serializer
+from rest_framework.views import APIView
 
 # Create your views here.
 
-@api_view(['GET', 'POST'])
-def product_list(request):
-    if request.method == 'GET':
+class ProductList(APIView):
+    def get(self, request):
         product_queryset = Product.objects.all()
         serializer = ProductSerializer(product_queryset, many=True)
         return Response(serializer.data)
-    elif request.method == 'POST':
+    def post(self, request):
         serializer = ProductSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         return Response(serializer.validated_data)
 
-@api_view(['GET', 'DELETE'])
-def product_details(request, id):
-    product = get_object_or_404(Product, pk=id)
-    if request.method == 'GET':
+
+class ProductDetails(APIView):
+    def get(self, request, id):
+        product = get_object_or_404(Product, pk=id)
         serializer = ProductSerializer(product)
         return Response(serializer.data)
-    elif request.method == 'DELETE':
+    def delete(self, request, id):
+        product = get_object_or_404(Product, pk=id)
         product.delete()
         return Response("Product has been deleted")
 
-@api_view(['GET', 'POST'])
-def collection_list(request):
-    if request.method == 'GET':
+
+class CollectionList(APIView):
+    def get(self, request):
         collections = Collection.objects.annotate(product_count=Count('product')).all()
         serializer = CollectionSerializer(collections, many=True)
         return Response(serializer.data)
-    elif request.method == 'POST':
+    def post(self, request):
         serializer = CollectionSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        # serializer.save()
         return Response(serializer.validated_data)
 
-@api_view(['GET', 'DELETE', 'PUT'])
-def collection_details(request, id):
-    collection = get_object_or_404(Collection.objects.annotate(product_count=Count('product')), pk=id)
-    if request.method == 'GET':
+
+class CollectionDetails(APIView):
+    def get(self, request, id):
+        collection = get_object_or_404(Collection.objects.annotate(product_count=Count('product')), pk=id)
         serializer = CollectionSerializer(collection)
         return Response(serializer.data)
-    elif request.method == 'DELETE':
+    def delete(self, request, id):
+        collection = get_object_or_404(Collection, pk=id)
         collection.delete()
         return Response("The collection has been deleted")
-    elif request.method == 'PUT':
+    def put(self, request, id):
+        collection = get_object_or_404(Collection, pk=id)
         serializer = CollectionSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
+        collection.title = serializer.validated_data['title']
+        collection.save()
+        return Response("Collection has been updated")
+
