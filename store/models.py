a0ad9e1c957c37c587c3b26716b7fe9b60d4e212
Masab Umair <Masab.Umair@mtp.tech>
2022-08-04 14:31:58 +0500

    Update Product model

--- store/models.py
+++ store/models.py
@@ -30,7 +30,7 @@ class Product(models.Model):
         validators=[MinValueValidator(1)])
     inventory = models.IntegerField(validators=[MinValueValidator(0)])
     last_update = models.DateTimeField(auto_now=True)
-    collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
+    collection = models.ForeignKey(Collection, on_delete=models.CASCADE)
     promotions = models.ManyToManyField(Promotion, blank=True)
 
     def __str__(self) -> str:
