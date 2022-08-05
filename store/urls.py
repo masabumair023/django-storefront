6ee49b07dedf58760ebec4847d552d052d9ff136
Masab Umair <sp18-bcs-023@students.cuisahiwal.edu.pk>
2022-08-05 13:02:21 +0500

    Viewsets (#8)
    * Implement viewsets for Product and Collection APIs
    
    * Add Routers

--- store/urls.py
+++ store/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path
 from . import views
+from rest_framework import routers
+
+router = routers.SimpleRouter()
+router.register('products', views.ProductViewSet)
+router.register('collections', views.CollectionViewSet)
 
 # URLConf
-urlpatterns = [
-    path('products/', views.ProductList.as_view()),
-    path('products/<int:id>/', views.ProductDetails.as_view()),
-    path('collections/', views.CollectionList.as_view()),
-    path('collections/<int:id>/', views.CollectionDetails.as_view()),
-]
+urlpatterns = router.urls
+
+# urlpatterns = [
+#     path('products/', views.ProductList.as_view()),
+#     path('products/<int:id>/', views.ProductDetails.as_view()),
+#     path('collections/', views.CollectionList.as_view()),
+#     path('collections/<int:id>/', views.CollectionDetails.as_view()),
+# ]
