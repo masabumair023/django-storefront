00cc424c26c34d237c754250d82361074025dc97
Masab Umair <Masab.Umair@mtp.tech>
2022-08-05 12:45:02 +0500

    Implement viewsets for Product and Collection APIs

--- store/views.py
+++ store/views.py
@@ -1,36 +1,19 @@
-from logging import raiseExceptions
-from django.shortcuts import get_object_or_404
 from django.db.models import Count
 
 from .models import Collection, Product
 from .serializer import CollectionSerializer, ProductSerializer
 
-from rest_framework.response import Response
-from rest_framework.decorators import api_view
-from rest_framework.views import APIView
-from rest_framework.mixins import ListModelMixin
-from rest_framework.generics import ListCreateAPIView
-from rest_framework.generics import RetrieveUpdateDestroyAPIView
-
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.viewsets import ModelViewSet
 # Create your views here.
 
-class ProductList(ListCreateAPIView):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
-
-
-class ProductDetails(RetrieveUpdateDestroyAPIView):
+class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     lookup_field = 'id'
 
 
-class CollectionList(ListCreateAPIView):
+class CollectionViewSet(ModelViewSet):
     queryset = Collection.objects.annotate(product_count=Count('product')).all()
-    serializer_class = CollectionSerializer
-
-
-class CollectionDetails(RetrieveUpdateDestroyAPIView):
-    queryset = Collection.objects.annotate(product_count=Count('product'))
     serializer_class = CollectionSerializer
     lookup_field = 'id'
