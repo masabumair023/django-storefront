dd9c7077e5a8a3201d6e60ca57927044a42cbf06
Masab Umair <Masab.Umair@mtp.tech>
2022-09-29 19:17:37 +0500

    Add settings for sending smtp emails

--- storefront/settings.py
+++ storefront/settings.py
@@ -161,3 +161,14 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'COERCE_DECIMAL_TO_STRING': False
 }
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'localhost'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_PORT = 2525
+DEFAULT_FROM_EMAIL = 'from@masabumair.com'
+
+ADMINS = [
+    ('Masab', 'admin@masab.com')
+]
