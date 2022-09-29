64162bda4e320c4371937948ae5e4df653a65df4
Masab Umair <Masab.Umair@mtp.tech>
2022-09-29 13:07:56 +0500

    Update admin for media management

--- store_custom/admin.py
+++ store_custom/admin.py
@@ -1,7 +1,7 @@
 from store.models import Product
 from django.contrib import admin
 from django.contrib.contenttypes.admin import GenericTabularInline
-from store.admin import ProductAdmin
+from store.admin import ProductAdmin, ProductImageInline
 from tags.models import TaggedItem
 
 
@@ -11,7 +11,7 @@ class TagInline(GenericTabularInline):
 
 
 class CustomProductAdmin(ProductAdmin):
-    inlines = [TagInline]
+    inlines = [TagInline, ProductImageInline]
 
 
 admin.site.unregister(Product)
