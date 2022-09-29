a95a3b8371f4ac2b169749e5760bd6feb8c914e9
Masab Umair <sp18-bcs-023@students.cuisahiwal.edu.pk>
2022-09-29 20:18:07 +0500

    Smtp emails (#16)
    * Add functionality for sending emails
    
    * Setup a dummy email template
    
    * Add settings for sending SMTP emails
    
    * Update requirements file

--- playground/views.py
+++ playground/views.py
@@ -1,5 +1,17 @@
+from django.core.mail import BadHeaderError
 from django.shortcuts import render
+from templated_mail.mail import BaseEmailMessage
 
 
 def say_hello(request):
+    try:
+
+        message = BaseEmailMessage(
+            template_name='emails/hello_email.html',
+            context={'name': 'Masab'}
+        )
+        message.send(['to@masab.com'])
+
+    except BadHeaderError:
+        pass
     return render(request, 'hello.html', {'name': 'Masab'})
