478d729d7988232d8465508621d7f195784a3b88
Masab Umair <sp18-bcs-023@students.cuisahiwal.edu.pk>
2022-10-11 18:50:10 +0500

    Run background tasks with celery (#17)
    * Add tasks to run in background
    
    * background tasks
    
    * Migration
    
    * import celery
    
    * Celery configurations

--- playground/views.py
+++ playground/views.py
@@ -1,17 +1,20 @@
-from django.core.mail import BadHeaderError
+# from django.core.mail import BadHeaderError
 from django.shortcuts import render
-from templated_mail.mail import BaseEmailMessage
+from .tasks import notify_customers
+# from templated_mail.mail import BaseEmailMessage
 
 
 def say_hello(request):
-    try:
 
-        message = BaseEmailMessage(
-            template_name='emails/hello_email.html',
-            context={'name': 'Masab'}
-        )
-        message.send(['to@masab.com'])
+    notify_customers.delay('Hello')
+    # try:
 
-    except BadHeaderError:
-        pass
+    #     message = BaseEmailMessage(
+    #         template_name='emails/hello_email.html',
+    #         context={'name': 'Masab'}
+    #     )
+    #     message.send(['to@masab.com'])
+
+    # except BadHeaderError:
+    #     pass
     return render(request, 'hello.html', {'name': 'Masab'})
