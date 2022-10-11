5b6502f9d24673929959ac96a6809f7d8ec5f1ce
Masab Umair <Masab.Umair@mtp.tech>
2022-10-11 18:52:28 +0500

    Wrote performance tests for APIs

---
+++ locustfiles/browse_products.py
@@ -0,0 +1,13 @@
+from locust import HttpUser, task, between
+from random import randint
+
+class WebsiteUser(HttpUser):
+    wait_time = between(1,5)
+
+
+    @task
+    def view_product(self):
+        product_id = randint(1,4)
+        self.client.get(
+            f'/store/products/{product_id}', 
+            name='store/products/:id')
