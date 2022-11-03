e8316c604c820f82691693c3a58c9074249fcee8
Masab Umair <Masab.Umair@mtp.tech>
2022-11-03 18:00:12 +0500

    Add functionality for logging

--- playground/views.py
+++ playground/views.py
@@ -1,12 +1,15 @@
+import logging
+import requests
 # from django.core.mail import BadHeaderError
 from django.shortcuts import render
-from django.core.cache import cache
-from django.views.decorators.cache import cache_page
+# from django.core.cache import cache
+# from django.views.decorators.cache import cache_page
 from django.utils.decorators import method_decorator
 from rest_framework.views import APIView
 # from .tasks import notify_customers
 # from templated_mail.mail import BaseEmailMessage
-import requests
+
+logger = logging.getLogger(__name__) #playground.views
 
 # @cache_page(5 * 60)
 # def say_hello(request):
@@ -27,8 +30,13 @@ import requests
 #     return render(request, 'hello.html', {'name': data})
 
 class HelloView(APIView):
-    @method_decorator(cache_page(5 * 60))
+    # @method_decorator(cache_page(5 * 60))
     def get(self, request):
-        response = requests.get('https://httpbin.org/delay/2')
-        data = response.json()
-        return render(request, 'hello.html', {'name': data})        
+        try:
+            logger.info("Calling httpbin")
+            response = requests.get('https://httpbin.org/delay/2')
+            logger.info("Received the response")
+            data = response.json()
+        except requests.ConnectionError:
+            logger.critical("httpbin is offline")
+        return render(request, 'hello.html', {'name': 'Masab'})        
