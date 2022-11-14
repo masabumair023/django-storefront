d38629a6138a2084a9354a8e2feab2046c633940
Masab Umair <sp18-bcs-023@students.cuisahiwal.edu.pk>
2022-11-14 21:19:58 +0500

    Development (#22)
    * Update logo
    
    * Add process file for deployment
    
    * Update host

--- storefront/settings/prod.py
+++ storefront/settings/prod.py
@@ -4,6 +4,6 @@ SECRET_KEY = env('SECRET_KEY')
 
 DEBUG = False
 
-ALLOWED_HOSTS = env('ALLOWED_HOSTS').split(',')
+ALLOWED_HOSTS = ['ecommerce-store-prod.herokuapp.com']
 
 # DJANGO_LOG_LEVEL = env('DJANGO_LOG_LEVEL')
